f2956214ed3741e67f89bc1ba1731b71dbd32b1b
mosesmbuthia <mbuthiamoses435@gmail.com>
2025-04-12 22:06:49 +0300

    refactored the styled components

--- Frontend/blog-it/src/Components/Navbar.jsx
+++ Frontend/blog-it/src/Components/Navbar.jsx
@@ -1,22 +1,23 @@
-import React from 'react'
-import { Box, Typography, AppBar, Toolbar, Button } from '@mui/material'
+import React from 'react';
+import { Box, Typography, AppBar, Toolbar, Button } from '@mui/material';
 import { styled } from '@mui/material/styles';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
 function Navbar() {
-    const StyledAppBar = styled(AppBar)(({ theme }) => ({
+    const StyledAppBar = styled(AppBar)({
         background: 'linear-gradient(45deg, #6B48FF 30%, #00DDEB 90%)',
         boxShadow: '0 3px 5px 2px rgba(107, 72, 255, .3)',
-    }));
+    });
 
     const Logo = styled('img')({
         height: '40px',
         marginRight: '16px',
         borderRadius: '5px',
-        overflow: 'hidden'
+        overflow: 'hidden',
     });
 
-    const StyledButton = styled(Button)(({ theme }) => ({
-        margin: theme.spacing(0, 1),
+    const StyledButton = styled(Button)({
+        margin: '0 8px',
         backgroundColor: 'rgba(255, 255, 255, 0.15)',
         '&:hover': {
             backgroundColor: 'rgba(255, 255, 255, 0.25)',
@@ -24,70 +25,48 @@ function Navbar() {
         color: '#fff',
         borderRadius: '25px',
         padding: '6px 20px',
-    }));
+    });
+
     return (
-        
+        <StyledAppBar position="static">
+            <Toolbar>
+                <Logo
+                    src="./blogit-logo.jpg"
+                    alt="Blogit Logo"
+                />
 
+                <Typography
+                    variant="h5"
+                    component={Link}
+                    to="/"
+                    sx={{
+                        flexGrow: 1,
+                        color: '#fff',
+                        textDecoration: 'none',
+                        fontWeight: 700,
+                        letterSpacing: '1px',
+                        '&:hover': {
+                            color: '#f0f0f0',
+                        },
+                    }}
+                >
+                    Blogit
+                </Typography>
 
-            <StyledAppBar position="static">
-                <Toolbar>
-                   
-                    <Logo
-                        src="https://assets.grok.com/users/9e145854-1ec4-49e9-8f53-03a8a858280c/generated/wAbW6W84WK7SjKbx/image.jpg"
-                        alt="Blogit Logo"
-                        
-                    />
+                <Box>
+                    <StyledButton component={Link} to="/about" color="secondary">
+                        Our Story
+                    </StyledButton>
+                    <StyledButton component={Link} to="/login" color="warning">
+                        Login
+                    </StyledButton>
+                    <StyledButton component={Link} to="/signup" variant="contained">
+                        Sign Up
+                    </StyledButton>
+                </Box>
+            </Toolbar>
+        </StyledAppBar>
+    );
+}
 
-                   
-                    <Typography
-                        variant="h5"
-                        component={Link}
-                        to="/"
-                        sx={{
-                            flexGrow: 1,
-                            color: '#fff',
-                            textDecoration: 'none',
-                            fontWeight: 700,
-                            letterSpacing: '1px',
-                            '&:hover': {
-                                color: '#f0f0f0'
-                            }
-                        }}
-                    >
-                        Blogit
-                    </Typography>
-
-                 
-                    <Box>
-                    <StyledButton
-                            component={Link}
-                            to="/about"
-                            color='secondary'
-                        >
-                            Our Story
-                        </StyledButton>
-
-                        <StyledButton
-                            component={Link}
-                            to="/login"
-                            color='warning'
-                        >
-                            Login
-                        </StyledButton>
-                        <StyledButton
-                            component={Link}
-                            to="/signup"
-                            variant="contained"
-                        >
-                            Sign Up
-                        </StyledButton>
-                    </Box>
-                </Toolbar>
-            </StyledAppBar>
-            );
-};
-        
-    
-
-
-export default Navbar
+export default Navbar;
