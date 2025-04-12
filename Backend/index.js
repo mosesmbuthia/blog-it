bb3b839a3eb962ef7c22a911b84c3f87ba42dda1
mosesmbuthia <mbuthiamoses435@gmail.com>
2025-04-12 20:08:50 +0300

    build register functionality and route to login endpoint

--- Backend/index.js
+++ Backend/index.js
@@ -1,15 +1,21 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcryptjs';
+import cors from 'cors';
+import validation from './middlewares/validation.js';
 
 const blog = express();
 const client = new PrismaClient();
 
 blog.use(express.json())
+blog.use(cors({
+    origin: "http://localhost:5174",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]
+}))
 
-blog.post("/auth/signup", async (req, res) => {
+blog.post("/auth/signup", validation, async (req, res) => {
     const { firstName, lastName, emailAddress, userName, password } = req.body;
-    
+
     const hashedPassword = await bcrypt.hash(password, 12);
     try {
         const newUser = await client.user.create({
@@ -27,7 +33,7 @@ blog.post("/auth/signup", async (req, res) => {
             data: newUser
         })
 
-    } catch(error) {
+    } catch (error) {
         console.error("Error signing up:", error);
         res.status(500).json({
             status: "error",
