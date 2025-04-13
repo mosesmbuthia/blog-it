8da79ae0e880210ac73af4ce66ba884195866f86
mosesmbuthia <mbuthiamoses435@gmail.com>
2025-04-14 01:30:56 +0300

    build the write page

--- Frontend/blog-it/src/App.jsx
+++ Frontend/blog-it/src/App.jsx
@@ -4,6 +4,7 @@ import Herosection from './Components/Herosection';
 import Signup from './Components/Signup';
 import Login from './Components/Login';
 import BlogListing from './Pages/BlogListing';
+import WritePage from './Pages/WriteBlog';
 // import privateRoutes from './Store/Private.jsx';
 import './index.css';
 
@@ -17,10 +18,11 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/my-blogs" element={<BlogListing />} />
+        <Route path="/write" element={<WritePage />} />
 
         {/* <Route element={<privateRoutes />}>
           <Route path="/explore" element={<ExplorePage />} />
-          <Route path="/write" element={<WritePage />} />
+          
          
           <Route path="/profile" element={<ProfilePage />} />
         </Route> */}
